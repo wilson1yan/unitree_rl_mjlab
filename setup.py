ac371c06efec79d03e2a18bdecbec0d550a4b79e
Wilson Yan <wilson1.yan@berkeley.edu>
2026-05-22 16:34:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup, find_packages
 
 # Minimum dependencies required prior to installation
 INSTALL_REQUIRES = [
-    "mjlab==1.2.0",
-    "mujoco-warp==3.5.0",
+    "mjlab==1.3.0",
+    "mujoco-warp==3.7.0",
 ]
 
 # Installation operation
